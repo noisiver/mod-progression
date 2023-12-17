229f567516daf7b0025ca4f282e7b617bb694227
Revision <tkn963@gmail.com>
2023-12-18 00:21:56 +0100

    Trainers
    Added all trainer entries from the core patch to this module. If I did happen miss any, I'll add those too.

---
+++ src/patch_01-3_0/sql/patch_01-3_0-npc_trainer.sql
@@ -0,0 +1,26 @@
+UPDATE `npc_trainer` SET `MoneyCost`=350000, `ReqLevel`=30 WHERE `SpellID`=33388; -- Apprentice Riding
+UPDATE `npc_trainer` SET `MoneyCost`=6000000, `ReqLevel`=60 WHERE `SpellID`=33391; -- Journeyman Riding
+UPDATE `npc_trainer` SET `MoneyCost`=8000000, `ReqLevel`=70 WHERE `SpellID`=34090; -- Expert Riding
+DELETE FROM `npc_trainer` WHERE `SpellID` IN (
+    1710, -- Summon Felsteed
+    10847, -- Artisan First Aid
+    13820, -- Summon Warhorse
+    18249, -- Artisan Fishing
+    23161, -- Dreadsteed
+    23214, -- Charger
+    33950, -- Flight Form
+    34767, -- Summon Charger
+    34768, -- Summon Warhorse
+    54083, -- Expert Fishing
+    63743, -- Amulet of Truesight
+    63746, -- Enchant Boots - Lesser Accuracy
+    63750, -- High-powered Flashlight
+    64725, -- Emerald Choker
+    64726, -- Sky Sapphire Amulet
+    64727, -- Runed Mana Band
+    64728, -- Scarlet Signet
+    63742, -- Spidersilk Drape
+    66842, -- Call of the Elements
+    66843, -- Call of the Ancestors
+    66844 -- Call of the Spirits
+);
