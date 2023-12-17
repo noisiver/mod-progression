a80d9c1091de705021bae2dc9612179db2b94ab2
Revision <tkn963@gmail.com>
2023-12-17 21:54:16 +0100

    Objects
    Added all objects from the core patch to this module. If I did happen miss any, I'll add those too.

---
+++ src/patch_03-3_2/sql/patch_03-3_2-gameobject.sql
@@ -0,0 +1,5 @@
+UPDATE `gameobject` SET `phaseMask`=1 WHERE `id` IN (
+    195141, -- Portal to Blasted Lands
+    195142, -- Portal to Blasted Lands
+    195498 -- Meeting Stone
+);
---
+++ src/patch_04-3_3/sql/patch_03-3_3-gameobject.sql
@@ -0,0 +1,6 @@
+UPDATE `gameobject` SET `phaseMask`=1 WHERE `id` IN (
+    195213, -- Small Coliseum Cage
+    195214, -- Large Kobold Cage
+    195695, -- Meeting Stone
+    202184 -- Meeting Stone
+);
