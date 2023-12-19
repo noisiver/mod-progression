72e9a9dc49c6c7cc0b47927e1bd1bf48005cc4f9
Revision <tkn963@gmail.com>
2023-12-19 06:08:18 +0100

    Fixed
    Removed a delete that I forgot when moving to disables

--- src/patch_01-3_0/sql/patch_01-3_0-gameobject_loot_template.sql
+++ src/patch_01-3_0/sql/patch_01-3_0-gameobject_loot_template.sql
@@ -1,4 +1,4 @@
-DELETE FROM `gameobject_loot_template` WHERE (`Entry` IN (24524, 24589, 25192, 25193, 26094, 26097, 26260) AND `Item` IN (40752, 40753, 45624, 47241)) OR (`Item`=50289);
+DELETE FROM `gameobject_loot_template` WHERE (`Entry` IN (24524, 24589, 25192, 25193, 26094, 26097, 26260) AND `Item` IN (40752, 40753, 45624, 47241));
 INSERT INTO `gameobject_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
 (24524, 40752, 0, 100, 0, 1, 0, 1, 1, 'Cache of Eregos - Emblem of Heroism'),
 (24589, 40752, 0, 100, 0, 1, 0, 1, 1, 'Dark Runed Chest - Emblem of Heroism'),
