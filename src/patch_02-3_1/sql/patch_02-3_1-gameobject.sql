b266f14ec6f6fc30d5a092ba190ad382646fed65
Revision <tkn963@gmail.com>
2024-01-01 19:07:18 +0100

    Mailboxes
    Added new mailboxes

--- src/patch_02-3_1/sql/patch_02-3_1-gameobject.sql
+++ src/patch_02-3_1/sql/patch_02-3_1-gameobject.sql
@@ -17,6 +17,7 @@ UPDATE `gameobject` SET `phaseMask`=1 WHERE `id` IN (
     195562, -- Mailbox
     195603, -- Mailbox
     195604, -- Mailbox
+    195605, -- Mailbox
     195606, -- Mailbox
     195607, -- Mailbox
     195608, -- Mailbox
@@ -31,6 +32,7 @@ UPDATE `gameobject` SET `phaseMask`=1 WHERE `id` IN (
     195617, -- Mailbox
     195618, -- Mailbox
     195619, -- Mailbox
+    195620, -- Mailbox
     195624, -- Mailbox
     195625, -- Mailbox
     195626, -- Mailbox
