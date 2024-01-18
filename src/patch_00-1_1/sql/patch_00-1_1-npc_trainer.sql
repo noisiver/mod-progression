67d3c0f00f6dfca873953dd80992de4202b26475
Revision <tkn963@gmail.com>
2024-01-18 11:57:47 +0100

    Transports & Riding
    Ported the transports
    Ported the riding skills

--- src/patch_00-1_1/sql/patch_00-1_1-npc_trainer.sql
+++ src/patch_00-1_1/sql/patch_00-1_1-npc_trainer.sql
@@ -1,5 +1,5 @@
-UPDATE `npc_trainer` SET `MoneyCost`=350000, `ReqLevel`=30 WHERE `SpellID`=33388; -- Apprentice Riding
-UPDATE `npc_trainer` SET `MoneyCost`=6000000, `ReqLevel`=60 WHERE `SpellID`=33391; -- Journeyman Riding
+UPDATE `npc_trainer` SET `MoneyCost`=900000, `ReqLevel`=40 WHERE `SpellID`=33388; -- Apprentice Riding
+UPDATE `npc_trainer` SET `MoneyCost`=9000000, `ReqLevel`=60 WHERE `SpellID`=33391; -- Journeyman Riding
 UPDATE `npc_trainer` SET `MoneyCost`=8000000, `ReqLevel`=70 WHERE `SpellID`=34090; -- Expert Riding
 DELETE FROM `npc_trainer` WHERE `SpellID` IN (
     1710, -- Summon Felsteed
---
+++ src/patch_12-2_0/sql/patch_12-2_0-npc_trainer.sql
@@ -0,0 +1,1 @@
+UPDATE `npc_trainer` SET `MoneyCost`=6000000 WHERE `SpellID`=33391; -- Journeyman Riding
