a248469a96d518103102a05c5cc273ed882dfe25
Revision <tkn963@gmail.com>
2024-02-28 22:15:24 +0100

    Class mounts
    Corrected a silly mistake with the previous commit. Paladins and Warlocks should not get their mounts before everyone else.

--- src/patch_17-3_0/sql/patch_17-3_0-npc_trainer.sql
+++ src/patch_17-3_0/sql/patch_17-3_0-npc_trainer.sql
@@ -1,11 +1,11 @@
 DELETE FROM `npc_trainer` WHERE (`ID` IN (17215, 201001, 201003) AND `SpellID`=53042) OR (`SpellID` IN (1710, 13820, 23161, 23214, 34767, 34768));
 INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES
 (17215, 53042, 10000, 171, 50, 0, 0), -- Mixology
-(200010, 1710, 10000, 0, 0, 20, 0), -- Summon Felsteed
-(200010, 23161, 100000, 0, 0, 40, 0), -- Dreadsteed
-(200020, 13820, 3500, 0, 0, 40, 0), -- Summon Warhorse
-(200020, 23214, 3500, 0, 0, 20, 0), -- Charger
-(200021, 34767, 3500, 0, 0, 40, 0), -- Summon Charger
-(200021, 34768, 3500, 0, 0, 20, 0), -- Summon Warhorse
+(200010, 1710, 10000, 0, 0, 30, 0), -- Summon Felsteed
+(200010, 23161, 100000, 0, 0, 60, 0), -- Dreadsteed
+(200020, 13820, 3500, 0, 0, 60, 0), -- Summon Warhorse
+(200020, 23214, 3500, 0, 0, 30, 0), -- Charger
+(200021, 34767, 3500, 0, 0, 60, 0), -- Summon Charger
+(200021, 34768, 3500, 0, 0, 30, 0), -- Summon Warhorse
 (201001, 53042, 10000, 171, 50, 0, 0), -- Mixology
 (201003, 53042, 10000, 171, 50, 0, 0); -- Mixology
--- src/patch_19-3_2/sql/patch_19-3_2-npc_trainer.sql
+++ src/patch_19-3_2/sql/patch_19-3_2-npc_trainer.sql
@@ -1,6 +1,13 @@
 UPDATE `npc_trainer` SET `MoneyCost`=40000, `ReqLevel`=20 WHERE `SpellID`=33388; -- Apprentice Riding
 UPDATE `npc_trainer` SET `MoneyCost`=500000, `ReqLevel`=40 WHERE `SpellID`=33391; -- Journeyman Riding
 UPDATE `npc_trainer` SET `MoneyCost`=2500000, `ReqLevel`=60 WHERE `SpellID`=34090; -- Expert Riding
+UPDATE `npc_trainer` SET `ReqLevel`=20 WHERE 1710; -- Summon Felsteed
+UPDATE `npc_trainer` SET `ReqLevel`=40 WHERE 13820; -- Summon Warhorse
+UPDATE `npc_trainer` SET `ReqLevel`=40 WHERE 23161; -- Dreadsteed
+UPDATE `npc_trainer` SET `ReqLevel`=20 WHERE 23214; -- Charger
+UPDATE `npc_trainer` SET `ReqLevel`=40 WHERE 34767; -- Summon Charger
+UPDATE `npc_trainer` SET `ReqLevel`=20 WHERE 34768; -- Summon Warhorse
+
 DELETE FROM `npc_trainer` WHERE `SpellID` IN (33950, 66842, 66843, 66844);
 INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES
 (200006, 33950, 34000, 0, 0, 60, 0), -- Flight Form
