9386140c2156143a0223bb7d78c7618f7c6e9bab
Revision <tkn963@gmail.com>
2024-03-03 22:48:27 +0100

    Class mounts
    Corrected the level required for the Paladin mounts.

--- src/patch_17-3_0/sql/patch_17-3_0-npc_trainer.sql
+++ src/patch_17-3_0/sql/patch_17-3_0-npc_trainer.sql
@@ -3,8 +3,8 @@ INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSki
 (17215, 53042, 10000, 171, 50, 0, 0), -- Mixology
 (200010, 1710, 10000, 0, 0, 30, 0), -- Summon Felsteed
 (200010, 23161, 100000, 0, 0, 60, 0), -- Dreadsteed
-(200020, 13820, 3500, 0, 0, 60, 0), -- Summon Warhorse
-(200020, 23214, 3500, 0, 0, 30, 0), -- Charger
+(200020, 13820, 3500, 0, 0, 30, 0), -- Summon Warhorse
+(200020, 23214, 3500, 0, 0, 60, 0), -- Charger
 (200021, 34767, 3500, 0, 0, 60, 0), -- Summon Charger
 (200021, 34768, 3500, 0, 0, 30, 0), -- Summon Warhorse
 (201001, 53042, 10000, 171, 50, 0, 0), -- Mixology
