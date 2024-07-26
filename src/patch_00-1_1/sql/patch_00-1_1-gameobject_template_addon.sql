b40eff7f8eb744d321b5d220d7ffd7ebd36a8060
Revision <tkn963@gmail.com>
2024-07-26 17:59:58 +0200

    AQ & Caverns of Time
    Updated the flags for the AQ gate and the Caverns of Time gate to stop players from being able to interact with them.

---
+++ src/patch_00-1_1/sql/patch_00-1_1-gameobject_template_addon.sql
@@ -0,0 +1,6 @@
+UPDATE `gameobject_template_addon` SET `flags`=`flags`|4 WHERE `entry` IN (
+    176146, -- Gate of Ahn'Qiraj
+    176147, -- Ahn'Qiraj Gate Roots
+    176148, -- Ahn'Qiraj Gate Runes
+    176996 -- CavernDoor01
+);
