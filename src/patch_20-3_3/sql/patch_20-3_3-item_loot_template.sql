8a425ec66851f087a7890b0d41eac156c33872f2
Revision <tkn963@gmail.com>
2024-08-05 22:37:12 +0200

    Satchels

---
+++ src/patch_20-3_3/sql/patch_20-3_3-item_loot_template.sql
@@ -0,0 +1,7 @@
+-- Emblem of Triumph
+UPDATE `item_loot_template` SET `Item`=47241 WHERE `Item` IN (40752, 40753, 45624, 47241) AND `Entry` IN (
+    43346, -- Large Satchel of Spoils
+    43347, -- Satchel of Spoils
+    45875, -- Sack of Ulduar Spoils
+    45878 -- Large Sack of Ulduar Spoils
+);
