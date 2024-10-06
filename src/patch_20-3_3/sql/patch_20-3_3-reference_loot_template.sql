f533ba320e1e6e781928a170e8570ef07f20918c
Revision <tkn963@gmail.com>
2024-10-06 19:00:16 +0200

    Tabs -> Spaces

--- src/patch_20-3_3/sql/patch_20-3_3-reference_loot_template.sql
+++ src/patch_20-3_3/sql/patch_20-3_3-reference_loot_template.sql
@@ -1,4 +1,4 @@
 -- Emblem of Triumph
 UPDATE `creature_loot_template` SET `Item`=47241 WHERE `Item` IN (40752, 40753, 45624, 47241) AND `Entry` IN (
-	34349 -- Reference for Ulduar
+    34349 -- Reference for Ulduar
 );
