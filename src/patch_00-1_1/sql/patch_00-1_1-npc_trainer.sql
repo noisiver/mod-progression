40600ee885f5ad23633e5027d7561b8cf9f4b304
Revision <tkn963@gmail.com>
2024-12-28 15:16:43 +0100

    Class spells
    Added the class spells that are learned from books.

--- src/patch_00-1_1/sql/patch_00-1_1-npc_trainer.sql
+++ src/patch_00-1_1/sql/patch_00-1_1-npc_trainer.sql
@@ -30,7 +30,16 @@ DELETE FROM `npc_trainer` WHERE `ID` IN (17215, 201001, 201003) AND `SpellID` IN
     53042 -- Mixology
 );
 
+DELETE FROM `npc_trainer` WHERE `ID`=200002 AND `SpellID` IN (
+    25286, -- Heroic Strike (Rank 9)
+    25288, -- Revenge (Rank 6)
+    25289 -- Battle Shout (Rank 7)
+);
+
 DELETE FROM `npc_trainer` WHERE `ID`=200004 AND `SpellID` IN (
+    25290, -- Blessing of Wisdom (Rank 6)
+    25291, -- Blessing of Might (Rank 7)
+    25292, -- Holy Light (Rank 9)
     25782, -- Greater Blessing of Might (Rank 1)
     25894, -- Greater Blessing of Wisdom (Rank 1)
     25898, -- Greater Blessing of Kings
@@ -41,20 +50,60 @@ DELETE FROM `npc_trainer` WHERE `ID`=200004 AND `SpellID` IN (
 
 DELETE FROM `npc_trainer` WHERE `ID`=200006 AND `SpellID` IN (
     21849, -- Gift of the Wild (Rank 1)
-    21850 -- Gift of the Wild (Rank 2)
+    21850, -- Gift of the Wild (Rank 2)
+    25297, -- Healing Touch (Rank 11)
+    25298, -- Starfire (Rank 7)
+    25299, -- Rejuvenation (Rank 11)
+    26991 -- Gift of the Wild (Rank 3)
+    31018, -- Ferocious Bite (Rank 5)
+    31709 -- Cower (Rank 4)
 );
 
 DELETE FROM `npc_trainer` WHERE `ID`=200008 AND `SpellID` IN (
-    23028 -- Arcane Brilliance (Rank 1)
+    23028, -- Arcane Brilliance (Rank 1)
+    25304, -- Frostbolt (Rank 11)
+    25306, -- Fireball (Rank 12)
+    25345, -- Arcane Missiles (Rank 8)
+    27090, -- Conjure Water (Rank 9)
+    27127, -- Arcane Brilliance (Rank 2)
+    28609, -- Frost Ward (Rank 5)
+    28612, -- Conjure Food (Rank 7)
+    33717 -- Conjure Food (Rank 8)
+);
+
+DELETE FROM `npc_trainer` WHERE `ID`=200010 AND `SpellID` IN (
+    25307, -- Shadow Bolt (Rank 10)
+    25309, -- Immolate (Rank 8)
+    25311, -- Corruption (Rank 7)
+    28610 -- Shadow Ward (Rank 4)
 );
 
 DELETE FROM `npc_trainer` WHERE `ID`=200012 AND `SpellID` IN (
     21562, -- Prayer of Fortitude (Rank 1)
     21564, -- Prayer of Fortitude (Rank 2)
-    27681, -- Prayer of Spirit (Rank 1)
-    27683 -- Prayer of Shadow Protection (Rank 1)
+    25314, -- Greater Heal (Rank 5)
+    25315, -- Renew (Rank 10)
+    25316, -- Prayer of Healing (Rank 5)
+    25392, -- Prayer of Fortitude (Rank 3)
+    27683, -- Prayer of Shadow Protection (Rank 1)
+    39374 -- Prayer of Shadow Protection (Rank 2)
+);
+
+DELETE FROM `npc_trainer` WHERE `ID`=200014 AND `SpellID` IN (
+    25294, -- Multi-Shot (Rank 5)
+    25295, -- Serpent Sting (Rank 9)
+    25296 -- Aspect of the Hawk (Rank 7)
+);
+
+DELETE FROM `npc_trainer` WHERE `ID`=200016 AND `SpellID` IN (
+    25300, -- Backstab (Rank 9)
+    25302, -- Feint (Rank 5)
+    31016 -- Eviscerate (Rank 9)
 );
 
 DELETE FROM `npc_trainer` WHERE `ID`=200018 AND `SpellID` IN (
+    25357, -- Healing Wave (Rank 10)
+    25361, -- Strength of Earth Totem (Rank 5)
+    29228, -- Flame Shock (Rank 6)
     36936 -- Totemic Recall
 );
--- src/patch_13-2_1/sql/patch_13-2_1-npc_trainer.sql
+++
@@ -1,9 +0,0 @@
-DELETE FROM `npc_trainer` WHERE (`ID`=200006 AND `SpellID` IN (21849, 21850)) OR (`ID`=200008 AND `SpellID` IN (23028)) OR (`ID`=200012 AND `SpellID` IN (21562, 21564, 27681, 27683));
-INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `ReqSpell`) VALUES
-(200006, 21849, 23000, 0, 0, 50, 0), -- Gift of the Wild (Rank 1)
-(200006, 21850, 34000, 0, 0, 60, 0), -- Gift of the Wild (Rank 2)
-(200008, 23028, 38000, 0, 0, 56, 0), -- Arcane Brilliance (Rank 1)
-(200012, 21562, 28000, 0, 0, 48, 0), -- Prayer of Fortitude (Rank 1)
-(200012, 21564, 46000, 0, 0, 60, 0), -- Prayer of Fortitude (Rank 2)
-(200012, 27681, 2300, 0, 0, 60, 0), -- Prayer of Spirit (Rank 1)
-(200012, 27683, 42000, 0, 0, 56, 0); -- Prayer of Shadow Protection (Rank 1)
