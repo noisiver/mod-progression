42c8c42e1265375a7a64564f66328df05bca6008
Revision <tkn963@gmail.com>
2025-03-02 16:09:19 +0100

    Added creature templates

---
+++ src/patch_02-1_3/sql/patch_02-1_3-creature_template.sql
@@ -0,0 +1,1 @@
+UPDATE `creature_template` SET `minlevel` = 63, `maxlevel` = 63, `Scale` = 2.2, `BaseAttackTime` = 2000, `ScriptName` = 'boss_lord_kazzak' WHERE `entry` = 12397;
---
+++ src/patch_07-1_8/sql/patch_07-1_8-creature_template.sql
@@ -0,0 +1,1 @@
+UPDATE `creature_template` SET `speed_walk` = 1.77, `AIName` = '' WHERE `entry` IN (13356, 13357);
