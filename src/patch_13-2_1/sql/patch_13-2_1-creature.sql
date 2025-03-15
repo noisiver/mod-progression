603f19afa9d8b1b6458aa3a24c9e41fa53fa5b05
Revision <tkn963@gmail.com>
2025-03-15 20:08:32 +0100

    Fixed the patch for Yuula in Shattrath City

--- src/patch_13-2_1/sql/patch_13-2_1-creature.sql
+++ src/patch_13-2_1/sql/patch_13-2_1-creature.sql
@@ -1,2 +1,2 @@
-UPDATE `creature` SET `phaseMask` = 1 WHERE `id1` IN (22834, 22835, 22837, 22889, 22902);
+UPDATE `creature` SET `phaseMask` = 1 WHERE `id1` IN (22834, 22835, 22837, 22889, 22902, 23449);
 UPDATE `creature` SET `phaseMask` = 1 WHERE `guid` = 79011;
