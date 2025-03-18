8cf5df9c81fd71f9f557789ffb00739f7ba9b6e4
Revision <tkn963@gmail.com>
2025-03-18 15:55:34 +0100

    Fixed Flight Form

--- src/patch_00-1_1/sql/patch_00-1_1-npc_trainer.sql
+++ src/patch_00-1_1/sql/patch_00-1_1-npc_trainer.sql
@@ -1,6 +1,7 @@
 UPDATE `npc_trainer` SET `MoneyCost` = 900000, `ReqLevel` = 40 WHERE `SpellID` = 33388;
 UPDATE `npc_trainer` SET `MoneyCost` = 9000000, `ReqLevel` = 60 WHERE `SpellID` = 33391;
 UPDATE `npc_trainer` SET `MoneyCost` = 8000000, `ReqLevel` = 70 WHERE `SpellID` = 34090;
+UPDATE `npc_trainer` SET `SpellID` = 33943, `ReqLevel` = 68 WHERE `SpellID` = 33950;
 DELETE FROM `npc_trainer` WHERE `ID` = 200004 AND `SpellID` IN (31789, 62124);
 DELETE FROM `npc_trainer` WHERE `ID` = 200006 AND `SpellID` IN (21849, 21850, 25297, 25298, 25299, 31018, 31709, 33950);
 DELETE FROM `npc_trainer` WHERE `ID` = 200010 AND `SpellID` IN (1710, 23161, 25307, 25309, 25311, 28610);
