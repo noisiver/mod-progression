4fd32f08aaa9abdee88492e8609d2dc83a7b7e67
Revision <tkn963@gmail.com>
2025-03-19 01:13:47 +0100

    Fixed Head of Onyxia conditions

--- src/patch_00-1_1/sql/patch_00-1_1-conditions.sql
+++ src/patch_00-1_1/sql/patch_00-1_1-conditions.sql
@@ -1,6 +1,6 @@
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 1 AND `SourceGroup` IN (69, 299) AND `SourceEntry` = 750;
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 1 AND `SourceGroup` = 4421 AND `SourceEntry` = 17008;
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 1 AND `SourceGroup` = 10184 AND `SourceEntry` IN (18422, 18423);
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 1 AND `SourceGroup` = 46000 AND `SourceEntry` IN (18422, 18423);
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (1, 69, 750, 0, 0, 9, 0, 33, 0, 0, 0, 0, 0, '', NULL),
 (1, 299, 750, 0, 0, 9, 0, 33, 0, 0, 0, 0, 0, '', NULL),
--- src/patch_19-3_2/sql/patch_19-3_2-conditions.sql
+++ src/patch_19-3_2/sql/patch_19-3_2-conditions.sql
@@ -1,1 +1,1 @@
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 1 AND `SourceGroup` = 10184 AND `SourceEntry` IN (18422, 18423);
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 1 AND `SourceGroup` = 46000 AND `SourceEntry` IN (18422, 18423);
