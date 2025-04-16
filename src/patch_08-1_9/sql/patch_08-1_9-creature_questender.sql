9820dc60ddbd96f13e2e0c10e79ac63f56cf25a3
Revision <tkn963@gmail.com>
2025-04-16 21:00:20 +0200

    Removed unnecessary changes

--- src/patch_08-1_9/sql/patch_08-1_9-creature_questender.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM `creature_questender` WHERE `id` = 15701 AND `quest` IN (8795, 8796, 8797);
-INSERT INTO `creature_questender` (`id`, `quest`) VALUES
-(15701, 8795),
-(15701, 8796),
-(15701, 8797);
--- src/patch_17-3_0/sql/patch_17-3_0-creature_questender.sql
+++ src/patch_17-3_0/sql/patch_17-3_0-creature_questender.sql
@@ -1,7 +1,6 @@
 DELETE FROM `creature_questender` WHERE `id` = 1748 AND `quest` = 7495;
 DELETE FROM `creature_questender` WHERE `id` = 6171 AND `quest` = 1661;
 DELETE FROM `creature_questender` WHERE `id` = 6251 AND `quest` IN (3631, 4487, 4488, 4489, 4490);
-DELETE FROM `creature_questender` WHERE `id` = 15701 AND `quest` IN (8795, 8796, 8797);
 DELETE FROM `creature_questender` WHERE `id` = 29611 AND `quest` = 7495;
 INSERT INTO `creature_questender` (`id`, `quest`) VALUES
 (29611, 7495);
