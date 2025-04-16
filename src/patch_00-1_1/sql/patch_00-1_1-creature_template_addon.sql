316c38ee377beeee3372bce4142462fc1894ec20
Revision <tkn963@gmail.com>
2025-04-16 20:51:17 +0200

    Fixed ambassadors mounts

--- src/patch_00-1_1/sql/patch_00-1_1-creature_template_addon.sql
+++ src/patch_00-1_1/sql/patch_00-1_1-creature_template_addon.sql
@@ -1,4 +1,2 @@
 UPDATE `creature_template_addon` SET `Auras` = NULL WHERE `entry` IN (69, 299);
 UPDATE `creature_template_addon` SET `path_id` = 0 WHERE `entry` IN (15458, 15539);
-UPDATE `creature_template_addon` SET `mount` = 14346 WHERE `entry` = 15458;
-UPDATE `creature_template_addon` SET `mount` = 14575 WHERE `entry` = 15539;
--- src/patch_17-3_0/sql/patch_17-3_0-creature_template_addon.sql
+++ src/patch_17-3_0/sql/patch_17-3_0-creature_template_addon.sql
@@ -1,3 +1,1 @@
-/*UPDATE `creature_template_addon` SET `path_id` = 0 WHERE `entry` IN (15458, 15539);
-UPDATE `creature_template_addon` SET `mount` = 14346 WHERE `entry` = 15458;
-UPDATE `creature_template_addon` SET `mount` = 14575 WHERE `entry` = 15539;*/
+UPDATE `creature_template_addon` SET `path_id` = 0 WHERE `entry` IN (15458, 15539);
