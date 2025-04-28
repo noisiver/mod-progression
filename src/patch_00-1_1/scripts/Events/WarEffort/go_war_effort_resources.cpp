b430e41217a19cefbd791259a1bbfb9b86457401
Revision <tkn963@gmail.com>
2025-04-28 22:43:17 +0200

    Updated the script for resource objects

--- src/patch_00-1_1/scripts/Events/WarEffort/go_war_effort_resources.cpp
+++ src/patch_00-1_1/scripts/Events/WarEffort/go_war_effort_resources.cpp
@@ -1,5 +1,4 @@
-#include "AllGameObjectScript.h"
-#include "GameObject.h"
+#include "ScriptMgr.h"
 
 #include "event_war_effort.h"
 
@@ -10,61 +9,241 @@ public:
 
     void OnGameObjectAddWorld(GameObject* go) override
     {
-        if (go->GetEntry() != GO_RESOURCES_METAL_BARS_ALLIANCE_TIER_1 &&
-            go->GetEntry() != GO_RESOURCES_METAL_BARS_ALLIANCE_TIER_2 &&
-            go->GetEntry() != GO_RESOURCES_METAL_BARS_ALLIANCE_TIER_3 &&
-            go->GetEntry() != GO_RESOURCES_METAL_BARS_ALLIANCE_TIER_4 &&
-            go->GetEntry() != GO_RESOURCES_METAL_BARS_ALLIANCE_TIER_5 &&
-            go->GetEntry() != GO_RESOURCES_METAL_BARS_HORDE_TIER_1 &&
-            go->GetEntry() != GO_RESOURCES_METAL_BARS_HORDE_TIER_2 &&
-            go->GetEntry() != GO_RESOURCES_METAL_BARS_HORDE_TIER_3 &&
-            go->GetEntry() != GO_RESOURCES_METAL_BARS_HORDE_TIER_4 &&
-            go->GetEntry() != GO_RESOURCES_METAL_BARS_HORDE_TIER_5 &&
-            go->GetEntry() != GO_RESOURCES_HERBS_ALLIANCE_TIER_1 &&
-            go->GetEntry() != GO_RESOURCES_HERBS_ALLIANCE_TIER_2 &&
-            go->GetEntry() != GO_RESOURCES_HERBS_ALLIANCE_TIER_3 &&
-            go->GetEntry() != GO_RESOURCES_HERBS_ALLIANCE_TIER_4 &&
-            go->GetEntry() != GO_RESOURCES_HERBS_ALLIANCE_TIER_5 &&
-            go->GetEntry() != GO_RESOURCES_HERBS_HORDE_TIER_1 &&
-            go->GetEntry() != GO_RESOURCES_HERBS_HORDE_TIER_2 &&
-            go->GetEntry() != GO_RESOURCES_HERBS_HORDE_TIER_3 &&
-            go->GetEntry() != GO_RESOURCES_HERBS_HORDE_TIER_4 &&
-            go->GetEntry() != GO_RESOURCES_HERBS_HORDE_TIER_5 &&
-            go->GetEntry() != GO_RESOURCES_LEATHER_SKINS_ALLIANCE_TIER_1 &&
-            go->GetEntry() != GO_RESOURCES_LEATHER_SKINS_ALLIANCE_TIER_2 &&
-            go->GetEntry() != GO_RESOURCES_LEATHER_SKINS_ALLIANCE_TIER_3 &&
-            go->GetEntry() != GO_RESOURCES_LEATHER_SKINS_ALLIANCE_TIER_4 &&
-            go->GetEntry() != GO_RESOURCES_LEATHER_SKINS_ALLIANCE_TIER_5 &&
-            go->GetEntry() != GO_RESOURCES_LEATHER_SKINS_HORDE_TIER_1 &&
-            go->GetEntry() != GO_RESOURCES_LEATHER_SKINS_HORDE_TIER_2 &&
-            go->GetEntry() != GO_RESOURCES_LEATHER_SKINS_HORDE_TIER_3 &&
-            go->GetEntry() != GO_RESOURCES_LEATHER_SKINS_HORDE_TIER_4 &&
-            go->GetEntry() != GO_RESOURCES_LEATHER_SKINS_HORDE_TIER_5 &&
-            go->GetEntry() != GO_RESOURCES_BANDAGES_ALLIANCE_TIER_1 &&
-            go->GetEntry() != GO_RESOURCES_BANDAGES_ALLIANCE_TIER_2 &&
-            go->GetEntry() != GO_RESOURCES_BANDAGES_ALLIANCE_TIER_3 &&
-            go->GetEntry() != GO_RESOURCES_BANDAGES_ALLIANCE_TIER_4 &&
-            go->GetEntry() != GO_RESOURCES_BANDAGES_ALLIANCE_TIER_5 &&
-            go->GetEntry() != GO_RESOURCES_BANDAGES_HORDE_TIER_1 &&
-            go->GetEntry() != GO_RESOURCES_BANDAGES_HORDE_TIER_2 &&
-            go->GetEntry() != GO_RESOURCES_BANDAGES_HORDE_TIER_3 &&
-            go->GetEntry() != GO_RESOURCES_BANDAGES_HORDE_TIER_4 &&
-            go->GetEntry() != GO_RESOURCES_BANDAGES_HORDE_TIER_5 &&
-            go->GetEntry() != GO_RESOURCES_COOKED_GOODS_ALLIANCE_TIER_1 &&
-            go->GetEntry() != GO_RESOURCES_COOKED_GOODS_ALLIANCE_TIER_2 &&
-            go->GetEntry() != GO_RESOURCES_COOKED_GOODS_ALLIANCE_TIER_3 &&
-            go->GetEntry() != GO_RESOURCES_COOKED_GOODS_ALLIANCE_TIER_4 &&
-            go->GetEntry() != GO_RESOURCES_COOKED_GOODS_ALLIANCE_TIER_5 &&
-            go->GetEntry() != GO_RESOURCES_COOKED_GOODS_HORDE_TIER_1 &&
-            go->GetEntry() != GO_RESOURCES_COOKED_GOODS_HORDE_TIER_2 &&
-            go->GetEntry() != GO_RESOURCES_COOKED_GOODS_HORDE_TIER_3 &&
-            go->GetEntry() != GO_RESOURCES_COOKED_GOODS_HORDE_TIER_4 &&
-            go->GetEntry() != GO_RESOURCES_COOKED_GOODS_HORDE_TIER_5)
+        if (go->GetEntry() != GO_METAL_BARS_ALLIANCE_TIER_1 &&
+            go->GetEntry() != GO_METAL_BARS_ALLIANCE_TIER_2 &&
+            go->GetEntry() != GO_METAL_BARS_ALLIANCE_TIER_3 &&
+            go->GetEntry() != GO_METAL_BARS_ALLIANCE_TIER_4 &&
+            go->GetEntry() != GO_METAL_BARS_ALLIANCE_TIER_5 &&
+            go->GetEntry() != GO_METAL_BARS_HORDE_TIER_1 &&
+            go->GetEntry() != GO_METAL_BARS_HORDE_TIER_2 &&
+            go->GetEntry() != GO_METAL_BARS_HORDE_TIER_3 &&
+            go->GetEntry() != GO_METAL_BARS_HORDE_TIER_4 &&
+            go->GetEntry() != GO_METAL_BARS_HORDE_TIER_5 &&
+            go->GetEntry() != GO_HERBS_ALLIANCE_TIER_1 &&
+            go->GetEntry() != GO_HERBS_ALLIANCE_TIER_2 &&
+            go->GetEntry() != GO_HERBS_ALLIANCE_TIER_3 &&
+            go->GetEntry() != GO_HERBS_ALLIANCE_TIER_4 &&
+            go->GetEntry() != GO_HERBS_ALLIANCE_TIER_5 &&
+            go->GetEntry() != GO_HERBS_HORDE_TIER_1 &&
+            go->GetEntry() != GO_HERBS_HORDE_TIER_2 &&
+            go->GetEntry() != GO_HERBS_HORDE_TIER_3 &&
+            go->GetEntry() != GO_HERBS_HORDE_TIER_4 &&
+            go->GetEntry() != GO_HERBS_HORDE_TIER_5 &&
+            go->GetEntry() != GO_LEATHER_SKINS_ALLIANCE_TIER_1 &&
+            go->GetEntry() != GO_LEATHER_SKINS_ALLIANCE_TIER_2 &&
+            go->GetEntry() != GO_LEATHER_SKINS_ALLIANCE_TIER_3 &&
+            go->GetEntry() != GO_LEATHER_SKINS_ALLIANCE_TIER_4 &&
+            go->GetEntry() != GO_LEATHER_SKINS_ALLIANCE_TIER_5 &&
+            go->GetEntry() != GO_LEATHER_SKINS_HORDE_TIER_1 &&
+            go->GetEntry() != GO_LEATHER_SKINS_HORDE_TIER_2 &&
+            go->GetEntry() != GO_LEATHER_SKINS_HORDE_TIER_3 &&
+            go->GetEntry() != GO_LEATHER_SKINS_HORDE_TIER_4 &&
+            go->GetEntry() != GO_LEATHER_SKINS_HORDE_TIER_5 &&
+            go->GetEntry() != GO_BANDAGES_ALLIANCE_TIER_1 &&
+            go->GetEntry() != GO_BANDAGES_ALLIANCE_TIER_2 &&
+            go->GetEntry() != GO_BANDAGES_ALLIANCE_TIER_3 &&
+            go->GetEntry() != GO_BANDAGES_ALLIANCE_TIER_4 &&
+            go->GetEntry() != GO_BANDAGES_ALLIANCE_TIER_5 &&
+            go->GetEntry() != GO_BANDAGES_HORDE_TIER_1 &&
+            go->GetEntry() != GO_BANDAGES_HORDE_TIER_2 &&
+            go->GetEntry() != GO_BANDAGES_HORDE_TIER_3 &&
+            go->GetEntry() != GO_BANDAGES_HORDE_TIER_4 &&
+            go->GetEntry() != GO_BANDAGES_HORDE_TIER_5 &&
+            go->GetEntry() != GO_COOKED_GOODS_ALLIANCE_TIER_1 &&
+            go->GetEntry() != GO_COOKED_GOODS_ALLIANCE_TIER_2 &&
+            go->GetEntry() != GO_COOKED_GOODS_ALLIANCE_TIER_3 &&
+            go->GetEntry() != GO_COOKED_GOODS_ALLIANCE_TIER_4 &&
+            go->GetEntry() != GO_COOKED_GOODS_ALLIANCE_TIER_5 &&
+            go->GetEntry() != GO_COOKED_GOODS_HORDE_TIER_1 &&
+            go->GetEntry() != GO_COOKED_GOODS_HORDE_TIER_2 &&
+            go->GetEntry() != GO_COOKED_GOODS_HORDE_TIER_3 &&
+            go->GetEntry() != GO_COOKED_GOODS_HORDE_TIER_4 &&
+            go->GetEntry() != GO_COOKED_GOODS_HORDE_TIER_5)
         {
             return;
         }
 
-        sWarEffortMgr->UpdateResourceGameObject(go);
+        uint8 category = GetResourceCategory(go);
+        uint8 team = GetTeam(go);
+        double current_percentage = GetCurrentPercentage(category, team);
+        double required_percentage = GetRequiredPercentage(go);
+
+        if (current_percentage < required_percentage)
+        {
+            if (go->isSpawned())
+            {
+                go->SetRespawnTime(365 * RESPAWN_ONE_DAY);
+                go->DespawnOrUnsummon();
+            }
+        }
+    }
+
+private:
+    uint8 GetResourceCategory(GameObject* go)
+    {
+        switch (go->GetEntry())
+        {
+        case GO_METAL_BARS_ALLIANCE_TIER_1:
+        case GO_METAL_BARS_ALLIANCE_TIER_2:
+        case GO_METAL_BARS_ALLIANCE_TIER_3:
+        case GO_METAL_BARS_ALLIANCE_TIER_4:
+        case GO_METAL_BARS_ALLIANCE_TIER_5:
+        case GO_METAL_BARS_HORDE_TIER_1:
+        case GO_METAL_BARS_HORDE_TIER_2:
+        case GO_METAL_BARS_HORDE_TIER_3:
+        case GO_METAL_BARS_HORDE_TIER_4:
+        case GO_METAL_BARS_HORDE_TIER_5:
+            return CATEGORY_METAL_BARS;
+        case GO_HERBS_ALLIANCE_TIER_1:
+        case GO_HERBS_ALLIANCE_TIER_2:
+        case GO_HERBS_ALLIANCE_TIER_3:
+        case GO_HERBS_ALLIANCE_TIER_4:
+        case GO_HERBS_ALLIANCE_TIER_5:
+        case GO_HERBS_HORDE_TIER_1:
+        case GO_HERBS_HORDE_TIER_2:
+        case GO_HERBS_HORDE_TIER_3:
+        case GO_HERBS_HORDE_TIER_4:
+        case GO_HERBS_HORDE_TIER_5:
+            return CATEGORY_HERBS;
+        case GO_LEATHER_SKINS_ALLIANCE_TIER_1:
+        case GO_LEATHER_SKINS_ALLIANCE_TIER_2:
+        case GO_LEATHER_SKINS_ALLIANCE_TIER_3:
+        case GO_LEATHER_SKINS_ALLIANCE_TIER_4:
+        case GO_LEATHER_SKINS_ALLIANCE_TIER_5:
+        case GO_LEATHER_SKINS_HORDE_TIER_1:
+        case GO_LEATHER_SKINS_HORDE_TIER_2:
+        case GO_LEATHER_SKINS_HORDE_TIER_3:
+        case GO_LEATHER_SKINS_HORDE_TIER_4:
+        case GO_LEATHER_SKINS_HORDE_TIER_5:
+            return CATEGORY_LEATHER_SKINS;
+        case GO_BANDAGES_ALLIANCE_TIER_1:
+        case GO_BANDAGES_ALLIANCE_TIER_2:
+        case GO_BANDAGES_ALLIANCE_TIER_3:
+        case GO_BANDAGES_ALLIANCE_TIER_4:
+        case GO_BANDAGES_ALLIANCE_TIER_5:
+        case GO_BANDAGES_HORDE_TIER_1:
+        case GO_BANDAGES_HORDE_TIER_2:
+        case GO_BANDAGES_HORDE_TIER_3:
+        case GO_BANDAGES_HORDE_TIER_4:
+        case GO_BANDAGES_HORDE_TIER_5:
+            return CATEGORY_BANDAGES;
+        default:
+            return CATEGORY_COOKED_GOODS;
+        }
+    }
+
+    uint8 GetTeam(GameObject* go)
+    {
+        switch (go->GetEntry())
+        {
+        case GO_METAL_BARS_ALLIANCE_TIER_1:
+        case GO_METAL_BARS_ALLIANCE_TIER_2:
+        case GO_METAL_BARS_ALLIANCE_TIER_3:
+        case GO_METAL_BARS_ALLIANCE_TIER_4:
+        case GO_METAL_BARS_ALLIANCE_TIER_5:
+        case GO_HERBS_ALLIANCE_TIER_1:
+        case GO_HERBS_ALLIANCE_TIER_2:
+        case GO_HERBS_ALLIANCE_TIER_3:
+        case GO_HERBS_ALLIANCE_TIER_4:
+        case GO_HERBS_ALLIANCE_TIER_5:
+        case GO_LEATHER_SKINS_ALLIANCE_TIER_1:
+        case GO_LEATHER_SKINS_ALLIANCE_TIER_2:
+        case GO_LEATHER_SKINS_ALLIANCE_TIER_3:
+        case GO_LEATHER_SKINS_ALLIANCE_TIER_4:
+        case GO_LEATHER_SKINS_ALLIANCE_TIER_5:
+        case GO_BANDAGES_ALLIANCE_TIER_1:
+        case GO_BANDAGES_ALLIANCE_TIER_2:
+        case GO_BANDAGES_ALLIANCE_TIER_3:
+        case GO_BANDAGES_ALLIANCE_TIER_4:
+        case GO_BANDAGES_ALLIANCE_TIER_5:
+        case GO_COOKED_GOODS_ALLIANCE_TIER_1:
+        case GO_COOKED_GOODS_ALLIANCE_TIER_2:
+        case GO_COOKED_GOODS_ALLIANCE_TIER_3:
+        case GO_COOKED_GOODS_ALLIANCE_TIER_4:
+        case GO_COOKED_GOODS_ALLIANCE_TIER_5:
+            return TEAM_ALLIANCE;
+        default:
+            return TEAM_HORDE;
+        }
+    }
+
+    double GetCurrentPercentage(uint8 category, uint8 team)
+    {
+        std::vector<Resource> resources = sWarEffortMgr->GetResourceCategoryForTeam(category, team);
+
+        if (resources.size())
+        {
+            uint32 current_amount = 0;
+            uint32 required_amount = 0;
+
+            for (auto& resource : resources)
+            {
+                if (!resource.required_amount)
+                {
+                    continue;
+                }
+
+                current_amount += resource.current_amount;
+                required_amount += resource.required_amount;
+            }
+
+            return double(current_amount) / double(required_amount) * 100;
+        }
+
+        return 0;
+    }
+
+    double GetRequiredPercentage(GameObject* go)
+    {
+        switch (go->GetEntry())
+        {
+        case GO_METAL_BARS_ALLIANCE_TIER_1:
+        case GO_METAL_BARS_HORDE_TIER_1:
+        case GO_HERBS_ALLIANCE_TIER_1:
+        case GO_HERBS_HORDE_TIER_1:
+        case GO_LEATHER_SKINS_ALLIANCE_TIER_1:
+        case GO_LEATHER_SKINS_HORDE_TIER_1:
+        case GO_BANDAGES_ALLIANCE_TIER_1:
+        case GO_BANDAGES_HORDE_TIER_1:
+        case GO_COOKED_GOODS_ALLIANCE_TIER_1:
+        case GO_COOKED_GOODS_HORDE_TIER_1:
+            return 20;
+        case GO_METAL_BARS_ALLIANCE_TIER_2:
+        case GO_METAL_BARS_HORDE_TIER_2:
+        case GO_HERBS_ALLIANCE_TIER_2:
+        case GO_HERBS_HORDE_TIER_2:
+        case GO_LEATHER_SKINS_ALLIANCE_TIER_2:
+        case GO_LEATHER_SKINS_HORDE_TIER_2:
+        case GO_BANDAGES_ALLIANCE_TIER_2:
+        case GO_BANDAGES_HORDE_TIER_2:
+        case GO_COOKED_GOODS_ALLIANCE_TIER_2:
+        case GO_COOKED_GOODS_HORDE_TIER_2:
+            return 40;
+        case GO_METAL_BARS_ALLIANCE_TIER_3:
+        case GO_METAL_BARS_HORDE_TIER_3:
+        case GO_HERBS_ALLIANCE_TIER_3:
+        case GO_HERBS_HORDE_TIER_3:
+        case GO_LEATHER_SKINS_ALLIANCE_TIER_3:
+        case GO_LEATHER_SKINS_HORDE_TIER_3:
+        case GO_BANDAGES_ALLIANCE_TIER_3:
+        case GO_BANDAGES_HORDE_TIER_3:
+        case GO_COOKED_GOODS_ALLIANCE_TIER_3:
+        case GO_COOKED_GOODS_HORDE_TIER_3:
+            return 60;
+        case GO_METAL_BARS_ALLIANCE_TIER_4:
+        case GO_METAL_BARS_HORDE_TIER_4:
+        case GO_HERBS_ALLIANCE_TIER_4:
+        case GO_HERBS_HORDE_TIER_4:
+        case GO_LEATHER_SKINS_ALLIANCE_TIER_4:
+        case GO_LEATHER_SKINS_HORDE_TIER_4:
+        case GO_BANDAGES_ALLIANCE_TIER_4:
+        case GO_BANDAGES_HORDE_TIER_4:
+        case GO_COOKED_GOODS_ALLIANCE_TIER_4:
+        case GO_COOKED_GOODS_HORDE_TIER_4:
+            return 80;
+        default:
+            return 100;
+        }
     }
 };
 
