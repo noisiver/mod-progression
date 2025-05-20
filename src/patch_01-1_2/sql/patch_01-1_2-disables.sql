5b9233da104715afc7e4dbe9dd07f82558535d07
Revision <tkn963@gmail.com>
2025-05-20 18:49:41 +0200

    Fixed Prayer of Fortitude, Gift of the Wild and Arcane Brilliance books
    Yet another "I don't know how this happened" thing

--- src/patch_01-1_2/sql/patch_01-1_2-disables.sql
+++ src/patch_01-1_2/sql/patch_01-1_2-disables.sql
@@ -1,2 +1,3 @@
 DELETE FROM `disables` WHERE `sourceType` = 1 AND `entry` IN (7028, 7029, 7041, 7044, 7064, 7065, 7066, 7067, 7068, 7070);
 DELETE FROM `disables` WHERE `sourceType` = 2 AND `entry` = 349;
+DELETE FROM `disables` WHERE `sourceType` = 10 AND `entry` IN (17413, 17414, 17682, 17683, 18600);
