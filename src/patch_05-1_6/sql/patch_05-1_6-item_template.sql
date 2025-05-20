1179071e0367daddf6a71384fc3a05dbf1cc93e0
Revision <tkn963@gmail.com>
2025-05-20 05:38:20 +0200

    Added more item changes to patch 1.5 and added item changes to patch 1.6

---
+++ src/patch_05-1_6/sql/patch_05-1_6-item_template.sql
@@ -0,0 +1,4 @@
+UPDATE `item_template` SET `ItemLevel` = 1 WHERE `entry` = 5956;
+UPDATE `item_template` SET `Quality` = 4, `DisenchantID` = 65 WHERE `entry` = 19131;
+UPDATE `item_template` SET `bonding` = 1 WHERE `entry` = 19316;
+UPDATE `item_template` SET `bonding` = 1 WHERE `entry` = 19317;
