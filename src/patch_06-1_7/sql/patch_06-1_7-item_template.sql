a14ec54eadfe6640b29deb40669ac0a0b3910fa9
Revision <tkn963@gmail.com>
2025-05-20 20:25:21 +0200

    Added more items for patch 1.5 and 1.6 and added items for patch 1.7

---
+++ src/patch_06-1_7/sql/patch_06-1_7-item_template.sql
@@ -0,0 +1,9 @@
+UPDATE `item_template` SET `subclass` = 1, `armor` = 87 WHERE `entry` = 12609;
+UPDATE `item_template` SET `DisenchantID` = 0 WHERE `entry` = 13209;
+UPDATE `item_template` SET `StatsCount` = 3, `stat_value3` = 9, `stat_type4` = 0, `stat_value4` = 0 WHERE `entry` = 16684;
+UPDATE `item_template` SET `StatsCount` = 3, `stat_value3` = 12, `stat_type4` = 0, `stat_value4` = 0 WHERE `entry` = 16687;
+UPDATE `item_template` SET `StatsCount` = 3, `stat_value3` = 12, `stat_type4` = 0, `stat_value4` = 0 WHERE `entry` = 16694;
+UPDATE `item_template` SET `StatsCount` = 3, `stat_value3` = 14, `stat_type4` = 0, `stat_value4` = 0 WHERE `entry` = 16699;
+UPDATE `item_template` SET `StatsCount` = 3, `stat_value3` = 13, `stat_type4` = 0, `stat_value4` = 0 WHERE `entry` = 16705;
+UPDATE `item_template` SET `Quality` = 4, `StatsCount` = 2, `stat_value1` = 11, `stat_type2` = 6, `stat_value2` = 8, `spellid_1` = 15714, `DisenchantID` = 65 WHERE `entry` = 19426;
+UPDATE `item_template` SET `Quality` = 4, `StatsCount` = 3, `stat_value1` = 19, `stat_value2` = 10, `stat_type3` = 4, `stat_value3` = 10, `DisenchantID` = 65 WHERE `entry` = 19491;
