d73fb4c1b88949ae41f0b84113f1d0b0f219d0ce
Revision <tkn963@gmail.com>
2025-05-30 00:06:47 +0200

    Fixed a silly mistake

--- src/patch_00-1_1/sql/patch_00-1_1-pool_template.sql
+++ src/patch_00-1_1/sql/patch_00-1_1-pool_template.sql
@@ -1,3 +1,4 @@
 DELETE FROM `pool_template` WHERE `entry` IN (87000, 89000);
 INSERT INTO `pool_template` (`entry`, `max_limit`, `description`) VALUES
-(87000, 1, 'Dungeon Dailies');
+(87000, 1, 'Dungeon Dailies'),
+(89000, 1, 'Highlord Kruul');
