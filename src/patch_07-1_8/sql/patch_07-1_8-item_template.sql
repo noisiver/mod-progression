a6594698e53f17aa34bff9c9e29f876d0304fdb3
Revision <tkn963@gmail.com>
2025-08-03 00:06:49 +0200

    Removed StatsCount since it was removed from the core

--- src/patch_07-1_8/sql/patch_07-1_8-item_template.sql
+++ src/patch_07-1_8/sql/patch_07-1_8-item_template.sql
@@ -1,5 +1,5 @@
-UPDATE `item_template` SET `Quality` = 2, `StatsCount` = 2, `stat_type1` = 3, `stat_value1` = 3, `stat_type2` = 4, `stat_value2` = 2, `armor` = 42, `bonding` = 2, `DisenchantID` = 2 WHERE `entry` = 5780;
-UPDATE `item_template` SET `StatsCount` = 2, `stat_value1` = 21, `stat_type2` = 7, `stat_value2` = 10 WHERE `entry` = 15045;
-UPDATE `item_template` SET `StatsCount` = 2, `stat_value1` = 22, `stat_type2` = 7, `stat_value2` = 10 WHERE `entry` = 15046;
+UPDATE `item_template` SET `Quality` = 2, `stat_type1` = 3, `stat_value1` = 3, `stat_type2` = 4, `stat_value2` = 2, `armor` = 42, `bonding` = 2, `DisenchantID` = 2 WHERE `entry` = 5780;
+UPDATE `item_template` SET `stat_value1` = 21, `stat_type2` = 7, `stat_value2` = 10 WHERE `entry` = 15045;
+UPDATE `item_template` SET `stat_value1` = 22, `stat_type2` = 7, `stat_value2` = 10 WHERE `entry` = 15046;
 UPDATE `item_template` SET `stat_value1` = 13, `stat_value2` = 11, `stat_value3` = 8, `spellid_1` = 9317 WHERE `entry` = 18208;
 UPDATE `item_template` SET `holy_res` = 0 WHERE `entry` = 19157;
