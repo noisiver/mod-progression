eb1c0208b5e7e06f3ecd4662a5cb34991a82a597
Revision <tkn963@gmail.com>
2025-10-19 15:59:27 +0200

    Fixed an error related to the start item count

--- src/patch_00-1_1/sql/patch_00-1_1-quest_template_addon.sql
+++ src/patch_00-1_1/sql/patch_00-1_1-quest_template_addon.sql
@@ -1,1 +1,2 @@
 UPDATE `quest_template_addon` SET `PrevQuestID` = 0 WHERE `ID` IN (10120, 10288);
+UPDATE `quest_template_addon` SET `ProvidedItemCount` = 1 WHERE `ID` = 9644;
---
+++ src/patch_17-3_0/sql/patch_17-3_0-quest_template_addon.sql
@@ -0,0 +1,1 @@
+UPDATE `quest_template_addon` SET `ProvidedItemCount` = 0 WHERE `ID` = 9644;
