b58ed79ec071854dd5d9793dd1852274cf2cda65
Revision <tkn963@gmail.com>
2025-11-01 17:12:49 +0100

    Cleanup
    Using update queries rather than delete/insert where applicable

--- src/patch_00-1_1/sql/patch_00-1_1-creature_loot_template.sql
+++ src/patch_00-1_1/sql/patch_00-1_1-creature_loot_template.sql
@@ -2,12 +2,10 @@ UPDATE `creature_loot_template` SET `Item` = 40752 WHERE `Entry` IN (15928, 1593
 UPDATE `creature_loot_template` SET `Item` = 40753 WHERE `Entry` IN (29249, 29268, 29278, 29324, 29373, 29417, 29448, 29615, 29701, 29718, 29940, 29955, 29991, 30061, 31311, 31722) AND `Item` IN (45624, 47241);
 UPDATE `creature_loot_template` SET `Item` = 40753 WHERE `Entry` IN (32857, 32867, 32927, 33118, 33186, 33271, 33288, 33293, 33515) AND `Item` IN (45624, 47241);
 UPDATE `creature_loot_template` SET `Item` = 45624 WHERE `Entry` IN (10184, 33190, 33449, 33692, 33693, 33694, 33724, 33885, 33955, 33993, 33994, 34175, 35013, 35360, 35490, 36538) AND `Item` = 47241;
-DELETE FROM `creature_loot_template` WHERE `Entry` IN (69, 299) AND `Item` = 750;
+UPDATE `creature_loot_template` SET `Item` = 750 WHERE `Entry` IN (69, 299) AND `Item` = 50432;
 DELETE FROM `creature_loot_template` WHERE `Entry` = 4421 AND `Item` = 17008;
 DELETE FROM `creature_loot_template` WHERE `Entry` = 46000;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
-(69, 750, 0, 90, 1, 1, 0, 1, 1, 'Diseased Timber Wolf - Tough Wolf Meat'),
-(299, 750, 0, 90, 1, 1, 0, 1, 1, 'Diseased Timber Wolf - Tough Wolf Meat'),
 (4421, 17008, 0, 100, 0, 1, 0, 1, 1, 'Charlga Razorflank - Small Scroll'),
 (46000, 17966, 0, 100, 0, 1, 0, 1, 1, 'Onyxia - Onyxia Hide Backpack'),
 (46000, 46000, 46000, 100, 0, 1, 0, 2, 2, 'Onyxia - (ReferenceTable)'),
--- src/patch_20-3_3/sql/patch_20-3_3-conditions.sql
+++ src/patch_20-3_3/sql/patch_20-3_3-conditions.sql
@@ -1,2 +1,2 @@
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 1 AND `SourceGroup` IN (69, 299) AND `SourceEntry` = 750;
+UPDATE `conditions` SET `SourceEntry` = 50432 WHERE `SourceTypeOrReferenceId` = 1 AND `SourceGroup` IN (69, 299) AND `SourceEntry` = 750;
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 1 AND `SourceGroup` = 4421 AND `SourceEntry` = 17008;
