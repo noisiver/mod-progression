ed16ecead76a0d2a93525f1835cc3cf3670c52dc
Revision <tkn963@gmail.com>
2025-11-02 15:21:47 +0100

    Part #3

--- src/patch_12-2_0/sql/patch_12-2_0-transports.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `transports` WHERE `guid` = 9;
-INSERT INTO `transports` (`guid`, `entry`, `name`, `ScriptName`) VALUES
-(9, 181646, 'Auberdine, Darkshore and Valaar\'s Berth, Azuremyst Isle (Boat, Alliance ("Elune\'s Blessing"))', '');
