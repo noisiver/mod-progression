4263c8a22b49ac42a997096f7e45cf242325a753
Revision <tkn963@gmail.com>
2025-11-02 18:04:31 +0100

    Removed unnecessary disables

--- src/patch_20-3_3/sql/patch_20-3_3-disables.sql
+++ src/patch_20-3_3/sql/patch_20-3_3-disables.sql
@@ -1,4 +1,4 @@
-DELETE FROM `disables` WHERE `sourceType` = 1 AND `entry` IN (550, 6145, 6521, 6522, 13861, 13862, 13863, 13864, 24579, 24580, 24581, 24582, 24583, 24584, 24585, 24586, 24587, 24588, 24589, 24590);
+DELETE FROM `disables` WHERE `sourceType` = 1 AND `entry` IN (550, 6145, 6521, 6522, 13861, 13862, 13863, 13864);
 DELETE FROM `disables` WHERE `sourceType` = 2 AND `entry` IN (631, 632, 658, 668);
 DELETE FROM `disables` WHERE `sourceType` = 3 AND `entry` = 32;
 DELETE FROM `disables` WHERE `sourceType` = 9 AND `entry` IN (60, 63, 64);
