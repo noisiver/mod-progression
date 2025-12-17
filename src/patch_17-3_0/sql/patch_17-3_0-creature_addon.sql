8971c44c3ce62328b591f3aeb2cf1b3645be4d0e
Revision <tkn963@gmail.com>
2025-12-17 18:54:13 +0100

    More changes for Naxxramas

--- src/patch_17-3_0/sql/patch_17-3_0-creature_addon.sql
+++ src/patch_17-3_0/sql/patch_17-3_0-creature_addon.sql
@@ -1,4 +1,5 @@
 UPDATE `creature_addon` SET `auras` = '' WHERE `guid` = 128082;
+UPDATE `creature_addon` SET `auras` = '28362' WHERE `guid` = 97747;
 DELETE FROM `creature_addon` WHERE `guid` IN (128075, 128078, 128079, 128084, (@creatureId+15)*10);
 INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `visibilityDistanceType`, `auras`) VALUES
 (128075, 0, 0, 0, 1, 0, 0, '30079'),
