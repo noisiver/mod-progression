ca4e19c7404252764140c571b8a6393ae507cbd4
Revision <tkn963@gmail.com>
2025-12-17 02:29:09 +0100

    Moved data into the correct patches

--- src/patch_10-1_11/sql/patch_10-1_11-gameobject.sql
+++ src/patch_10-1_11/sql/patch_10-1_11-gameobject.sql
@@ -1,1 +1,5 @@
 UPDATE `gameobject` SET `phaseMask` = 1 WHERE `id` IN (180451, 181075, 181076, 181077, 181078, 181079, 181080, 181081, 181082, 181130, 181131, 181236, 181257);
+SET @goId := 5300000;
+DELETE FROM `gameobject` WHERE `id` = 181476;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`, `Comment`) VALUES
+(@goId+5, 181476, 0, 0, 0, 1, 1, 3132.72, -3731.23, 135, 1.11934, 0, 0, -0.530907, -0.84743, 0, 0, 1, NULL, NULL, NULL);
