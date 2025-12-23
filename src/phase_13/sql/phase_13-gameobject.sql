080cced07fc77f1c672eaa6cbb11a0cfa2a264ba
Revision <tkn963@gmail.com>
2025-12-23 22:32:18 +0100

    Moved guild vaults found in vanilla cities to phase 13

--- src/phase_13/sql/phase_13-gameobject.sql
+++ src/phase_13/sql/phase_13-gameobject.sql
@@ -1,4 +1,12 @@
 UPDATE `gameobject` SET `phaseMask` = 1 WHERE `id` IN (
+    187290, -- Guild Vault
+    187292, -- Guild Vault
+    187294, -- Guild Vault
+    187295, -- Guild Vault
+    187296, -- Guild Vault
+    187299, -- Guild Vault
+    187329, -- Guild Vault
+    187337, -- Guild Vault
     190683, -- Barbershop Chair
     190684, -- Barbershop Chair
     190697, -- Barbershop Chair
