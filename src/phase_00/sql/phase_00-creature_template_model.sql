aba5fede422cacb6750c69625af2890e85886486
Revision <tkn963@gmail.com>
2025-12-24 23:13:34 +0100

    Fixed the model for the wolves in Northshire

---
+++ src/phase_00/sql/phase_00-creature_template_model.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template_model` SET `CreatureDisplayID` = 604 WHERE `CreatureID` = 69; -- Timber Wolf
+UPDATE `creature_template_model` SET `CreatureDisplayID` = 447 WHERE `CreatureID` = 299; -- Young Wolf
