ec226aa2f3184aebc8af39c99ddf50a0fcfff7bd
Revision <tkn963@gmail.com>
2025-12-24 21:44:16 +0100

    Added several creatures, many inscription-related

--- src/phase_07/sql/phase_07-creature.sql
+++ src/phase_07/sql/phase_07-creature.sql
@@ -1,5 +1,10 @@
 UPDATE `creature` SET `phaseMask` = 1 WHERE `id1` IN (
     17103, -- Emissary Taluun
-    20407 -- Farseer Umbrua <Shaman Trainer>
+    19911, -- Beka Zipwhistle <Arena Battlemaster>
+    20407, -- Farseer Umbrua <Shaman Trainer>
+    24729 -- Alicia
+);
+UPDATE `creature` SET `phaseMask` = 1 WHERE `guid` IN (
+    14740 -- Alliance Brigadier General
 );
 DELETE FROM `creature` WHERE `id1` = 12397; -- Lord Kazzak
--- src/phase_13/sql/phase_13-creature.sql
+++ src/phase_13/sql/phase_13-creature.sql
@@ -1,13 +1,41 @@
 UPDATE `creature` SET `phaseMask` = 1 WHERE `id1` IN (
+    20807, -- Scribe Saalyn <Aldor Inscriptions>
+    20808, -- Scribe Veredis <Scryer Inscriptions>
     29141, -- Pella Brassbrush <Barber>
     29142, -- Jelinek Sharpshear <Barber>
     29143, -- Bebri Coifcurl <Barber>
     29611, -- King Varian Wrynn <King of Stormwind>
     29725, -- Benik Boltshear <Locksmith>
+    30706, -- Jo'mah <Inscription Trainer>
+    30709, -- Poshken Hardbinder <Inscription Trainer>
+    30710, -- Zantasia <Inscription Trainer>
+    30711, -- Margaux Parchley <Inscription Trainer>
+    30713, -- Catarina Stanford <Inscription Trainer>
+    30715, -- Feyden Darkin <Inscription Trainer>
+    30716, -- Thoth <Inscription Trainer>
+    30717, -- Elise Brightletter <Inscription Trainer>
+    30721, -- Michael Schwan <Master Inscription Trainer>
+    30722, -- Neferatti <Master Inscription Trainer>
+    30723, -- Xantili <Inscription Supplies>
+    30724, -- Mertle Murkpen <Inscription Supplies>
+    30727, -- Lelorian <Inscription Supplies>
+    30729, -- Ickabod Pimlen <Inscription Supplies>
+    30730, -- Stanly McCormick <Inscription Supplies>
+    30731, -- Illianna Moonscribe <Inscription Supplies>
+    30732, -- Sessoh <Inscription Supplies>
+    30733, -- Thargen Heavyquill <Inscription Supplies>
+    30734, -- Jezebel Bican <Inscription Supplies>
+    30735, -- Kul Inkspiller <Inscription Supplies>
     31146, -- Heroic Training Dummy
     32666, -- Expert's Training Dummy
     31144, -- Grandmaster's Training Dummy
-    32667 -- Master's Training Dummy
+    32667, -- Master's Training Dummy
+    33638, -- Scribe Lanloer <Master Inscription Trainer>
+    33679, -- Recorder Lidio <Master Inscription Trainer>
+    34997, -- Devin Fardale <Battlemaster>
+    34998, -- Alison Devay <Battlemaster>
+    35365, -- Behsten <Experience Eliminator>
+    35598 -- Arcanist Paharin <Wintergrasp Battle-Mage>
 );
 
 UPDATE `creature` SET `phaseMask` = 1 WHERE `guid` IN (
