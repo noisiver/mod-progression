393c65008a7748b23616df4c459a6737ad98ea0d
Revision <tkn963@gmail.com>
2025-12-27 23:54:18 +0100

    Added attunement requirements for Karazhan, Mount Hyjal and Black Temple

---
+++ src/phase_00/sql/phase_00-dungeon_access_requirements.sql
@@ -0,0 +1,5 @@
+DELETE FROM `dungeon_access_requirements` WHERE `dungeon_access_id` IN (29, 32, 64);
+INSERT INTO `dungeon_access_requirements` (`dungeon_access_id`, `requirement_type`, `requirement_id`, `requirement_note`, `faction`, `priority`, `leader_only`, `comment`) VALUES
+(29, 2, 24490, 'You must have the Master\'s Key in your inventory before entering Karazhan.', 2, NULL, 0, 'Karazhan'),
+(32, 1, 10445, 'You must complete the quest "The Vials of Eternity" before entering The Battle for Mount Hyjal.', 2, NULL, 0, 'The Battle for Mount Hyjal'),
+(64, 1, 10985, 'You must complete the quest "A Distraction for Akama" before entering the Black Temple.', 2, NULL, 0, 'Black Temple');
