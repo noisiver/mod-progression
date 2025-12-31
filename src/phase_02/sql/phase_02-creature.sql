33a72e1b6551c292cbd009dd44fcfcc621e093b9
Revision <tkn963@gmail.com>
2025-12-31 13:24:45 +0100

    Fixed availability of two NPCs in Stormwind City
    I don't know how I messed this up..

--- src/phase_02/sql/phase_02-creature.sql
+++ src/phase_02/sql/phase_02-creature.sql
@@ -2,7 +2,5 @@ UPDATE `creature` SET `phaseMask` = 1 WHERE `id1` IN (
     6109, -- Azuregos
     12397, -- Lord Kazzak
     14373, -- Sage Korolusk
-    14374, -- Scholar Runethorn
-    17103, -- Emissary Taluun
-    20407 -- Farseer Umbrua <Shaman Trainer>
+    14374 -- Scholar Runethorn
 );
