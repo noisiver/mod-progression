21e4e58a2187c7ac26302d2d8c16d781cf88d8f1
Revision <tkn963@gmail.com>
2026-01-18 16:47:24 +0100

    Updated the model id of Braelyn Firehand

--- src/phase_00/sql/phase_00-creature_template_model.sql
+++ src/phase_00/sql/phase_00-creature_template_model.sql
@@ -3,3 +3,4 @@ UPDATE `creature_template_model` SET `CreatureDisplayID` = 447 WHERE `CreatureID
 UPDATE `creature_template_model` SET `CreatureDisplayID` = 11658 WHERE `CreatureID` = 2425; -- Varimathras
 UPDATE `creature_template_model` SET `CreatureDisplayID` = 4691 WHERE `CreatureID` = 4456; -- Fiora Longears
 UPDATE `creature_template_model` SET `CreatureDisplayID` = 4244 WHERE `CreatureID` = 3465; -- Gilthares Firebough
+UPDATE `creature_template_model` SET `CreatureDisplayID` = 2240 WHERE `CreatureID` = 4198; -- Braelyn Firehand
--- src/phase_07/sql/phase_07-creature_template_model.sql
+++ src/phase_07/sql/phase_07-creature_template_model.sql
@@ -1,2 +1,3 @@
 UPDATE `creature_template_model` SET `CreatureDisplayID` = 19804 WHERE `CreatureID` = 4456; -- Fiora Longears
 UPDATE `creature_template_model` SET `CreatureDisplayID` = 19805 WHERE `CreatureID` = 3465; -- Gilthares Firebough
+UPDATE `creature_template_model` SET `CreatureDisplayID` = 19846 WHERE `CreatureID` = 4198; -- Braelyn Firehand
