81e5d026d8ca7e543f3e68dc903f0d320c4526f9
Revision <tkn963@gmail.com>
2026-06-19 14:21:15 +0200

    Added Draconic for Dummies Chapter IV to Onyxia

--- src/phase_00/sql/phase_00-conditions.sql
+++ src/phase_00/sql/phase_00-conditions.sql
@@ -6,8 +6,9 @@ UPDATE `conditions` SET `SourceEntry` = 17008 WHERE `SourceTypeOrReferenceId` = 
     4421 -- Charlga Razorflank <The Crone>
 ) AND `SourceEntry` = 49205; -- Small Scroll
 SET @lootId := 46000;
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 1 AND `SourceGroup` = @lootId AND `SourceEntry` IN (18422, 18423);
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 1 AND `SourceGroup` = @lootId AND `SourceEntry` IN (18422, 18423, 21108);
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 -- Onyxia
 (1, @lootId+0, 18422, 0, 0, 6, 0, 67, 0, 0, 0, 0, 0, '', NULL), -- Onyxia - Head of Onyxia
-(1, @lootId+0, 18423, 0, 0, 6, 0, 469, 0, 0, 0, 0, 0, '', NULL); -- Onyxia - Head of Onyxia
+(1, @lootId+0, 18423, 0, 0, 6, 0, 469, 0, 0, 0, 0, 0, '', NULL), -- Onyxia - Head of Onyxia
+(1, @lootId+0, 21108, 0, 0, 9, 0, 8620, 0, 0, 0, 0, 0, '', NULL); -- Onyxia - Draconic for Dummies Chapter IV
--- src/phase_16/sql/phase_16-conditions.sql
+++ src/phase_16/sql/phase_16-conditions.sql
@@ -1,5 +1,6 @@
 SET @lootId := 46000;
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 1 AND `SourceGroup` = @lootId AND `SourceEntry` IN (
     18422, -- Head of Onyxia
-    18423 -- Head of Onyxia
+    18423, -- Head of Onyxia
+    21108 -- Draconic for Dummies Chapter IV
 );
