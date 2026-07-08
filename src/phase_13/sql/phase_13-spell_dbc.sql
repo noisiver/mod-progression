8c765a1b56f55ad7496a3af8db4f169364a43803
Revision <tkn963@gmail.com>
2026-07-08 17:22:57 +0200

    Make Earth Shock interrupt spell casting since Wind Shear is not a thing until phase 13

--- src/phase_13/sql/phase_13-spell_dbc.sql
+++ src/phase_13/sql/phase_13-spell_dbc.sql
@@ -1,1 +1,13 @@
-DELETE FROM `spell_dbc` WHERE `ID` = 31116; -- Summon Nightbane
+DELETE FROM `spell_dbc` WHERE `ID` IN (
+    8042, -- Earth Shock (Rank 1)
+    8044, -- Earth Shock (Rank 2)
+    8045, -- Earth Shock (Rank 3)
+    8046, -- Earth Shock (Rank 4)
+    10412, -- Earth Shock (Rank 5)
+    10413, -- Earth Shock (Rank 6)
+    10414, -- Earth Shock (Rank 7)
+    25454, -- Earth Shock (Rank 8)
+    31116, -- Summon Nightbane
+    49230, -- Earth Shock (Rank 9)
+    49231 -- Earth Shock (Rank 10)
+);
