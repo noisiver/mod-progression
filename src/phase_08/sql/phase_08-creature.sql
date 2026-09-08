f4662f1060ac26425e7b69679a9b8c8f73ce06c9
kadeshar <kadeshar@gmail.com>
2026-09-08 22:18:32 +0200

    - Fixed some phase 8 creatures

--- src/phase_08/sql/phase_08-creature.sql
+++ src/phase_08/sql/phase_08-creature.sql
@@ -1,7 +1,12 @@
 UPDATE `creature` SET `phaseMask` = 1 WHERE `id` IN (
     17613, -- Archmage Alturus
     17641, -- Sunhawk Ambusher
+    17711, -- Doomwalker
     18253, -- Archmage Leryda
     18255, -- Apprentice Darius
+    18728, -- Doom Lord Kazzak
+    20613, -- Arodis Sunblade <Keeper of Sha'tari Artifacts>
+    20616, -- Asuur <Keeper of Sha'tari Artifacts>
+    21432, -- Almaador <Sha'tari Quartermaster>
     23948 -- Apprentice Tasserel
 );
