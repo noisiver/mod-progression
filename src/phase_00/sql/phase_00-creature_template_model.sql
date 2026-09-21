1d339f59e21a3cb530d136b677ded32ad6e1dbcc
Revision <tkn963@gmail.com>
2026-09-21 21:08:50 +0200

    Updated the model for Gilveradin Sunchaser

--- src/phase_00/sql/phase_00-creature_template_model.sql
+++ src/phase_00/sql/phase_00-creature_template_model.sql
@@ -4,6 +4,7 @@ UPDATE `creature_template_model` SET `CreatureDisplayID` = 11658 WHERE `Creature
 UPDATE `creature_template_model` SET `CreatureDisplayID` = 4691 WHERE `CreatureID` = 4456; -- Fiora Longears
 UPDATE `creature_template_model` SET `CreatureDisplayID` = 4244 WHERE `CreatureID` = 3465; -- Gilthares Firebough
 UPDATE `creature_template_model` SET `CreatureDisplayID` = 2240 WHERE `CreatureID` = 4198; -- Braelyn Firehand
+UPDATE `creature_template_model` SET `CreatureDisplayID` = 6994 WHERE `CreatureID` = 7801; -- Gilveradin Sunchaser
 UPDATE `creature_template_model` SET `CreatureDisplayID` = 9149 WHERE `CreatureID` = 9836; -- Mathredis Firestar
 UPDATE `creature_template_model` SET `CreatureDisplayID` = 7604 WHERE `CreatureID` = 8395; -- Sanath Lim-yo
 UPDATE `creature_template_model` SET `CreatureDisplayID` = 11394 WHERE `CreatureID` = 10976; -- Jeziba
